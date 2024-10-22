ae835c8d77767ac5aa345ae1afd7e95555969e5b
Kim Minh Thang <thangved02@gmail.com>
2024-10-22 15:37:03 +0700

    fix: add fallback values for COHERE_API_KEY

--- api/dependencies.py
+++ api/dependencies.py
@@ -3,7 +3,7 @@ import os
 import chromadb
 from langchain_cohere import CohereEmbeddings
 
-COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY", "")
 
 CHROMA_HOST = os.getenv("CHROMA_HOST", "localhost")
 CHROMA_PORT = os.getenv("CHROMA_PORT", 8080)
